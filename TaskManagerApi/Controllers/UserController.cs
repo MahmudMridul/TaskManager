f8592e4d7eeb8afa9aa1fc98bd70c760e169e71e
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-11-27 18:15:04 +0600

    feat: Add user signup api

---
+++ TaskManagerApi/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.JSInterop.Infrastructure;
+using TaskManagerApi.Models;
+using TaskManagerApi.Models.Dtos;
+
+namespace TaskManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IConfiguration _config;
+
+        public UserController(UserManager<User> userManager, SignInManager<User> signInManager, IConfiguration config)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _config = config;
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp([FromBody] UserRegistrationDto dto)
+        {
+            User user = new User
+            {
+                UserName = dto.Email,
+                Email = dto.Email,
+                FirstName = dto.FirstName,
+                LastName = dto.LastName,
+            };
+
+            var result = await _userManager.CreateAsync(user, dto.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "User");
+                return Ok(new { Message = "User registered successfully" });
+            }
+
+            return BadRequest(result.Errors);
+        }
+    }
+}
