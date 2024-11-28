1cf62e7651e2a69491cad61a4f5949a46a5785ef
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-11-28 09:28:51 +0600

    new: add helper classes

---
+++ TaskManagerApi/Helpers/ResponseHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using TaskManagerApi.Models;
+
+namespace TaskManagerApi.Helpers
+{
+    public class ResponseHelper
+    {
+        public static ApiResponse CreateResponse(HttpStatusCode statusCode, object ? data = default, bool success = false, string msg = "", List<string>? errors = null)
+        {
+            return new ApiResponse
+            {
+                Data = data,
+                Success = success,
+                Message = msg,
+                StatusCode = statusCode,
+                Errors = errors
+            };
+        }
+    }
+}
---
+++ TaskManagerApi/Models/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace TaskManagerApi.Models
+{
+    public class ApiResponse
+    {
+        public object? Data { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public HttpStatusCode StatusCode { get; set; }
+        public List<string>? Errors { get; set; }
+
+    }
+}
